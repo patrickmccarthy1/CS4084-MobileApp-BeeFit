f20ca1aea7b0c416d0eebbafcea506dadb8adbe6
Brian Reale <91121209+brianreale@users.noreply.github.com>
2022-04-11 13:36:01 +0100

    CS4084-MobileApp-Brianeale15169693

---
+++ app/src/main/java/ie/ul/stopwatch/MainActivity.java
@@ -0,0 +1,89 @@
+package ie.ul.stopwatch;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds;
+    private boolean timing;
+    private boolean wasTiming;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+    public void startTime(View view){
+       timing =true;
+    }
+    public void stopTime(View view){
+       timing =false;
+    }
+    public void resetTime(View view){
+        timing =false;
+        seconds =0;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasTiming) {
+            timing = true;
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (timing) {
+            wasTiming = true;
+            timing = false;
+        } else {
+            wasTiming = false;
+            timing = false;
+        }
+    }
+
+    private void init() {
+        TextView timeView = findViewById(R.id.timer);
+        Handler handler = new Handler();
+        handler.post(new Runnable(){
+
+            @Override
+            public void run(){
+                int hrs;
+                hrs = seconds / 3600;
+                int mins;
+                mins = (seconds % 3600)/60;
+                int secs;
+                secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(),
+                                               "%d:%02d:%02d",
+                                                 hrs, mins, secs);
+                timeView.setText(time);
+
+                if (!timing) {
+                } else {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+                }
+
+        });
+    }
+
+}
