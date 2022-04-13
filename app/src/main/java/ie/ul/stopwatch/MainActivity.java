f6c7f66f24329ccfed09520d24c7933af1ce4a08
Brian Reale <91121209+brianreale@users.noreply.github.com>
2022-04-13 17:17:30 +0100

    Third-Stopwatch-Commit CS4084-MobileApp-BrianReale15169693

--- app/src/main/java/ie/ul/stopwatch/MainActivity.java
+++ app/src/main/java/ie/ul/stopwatch/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     //declaring variables
     private int seconds;
     private boolean timing;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +22,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startTime(View view){
+        this.view = view;
         //starts time
-       timing =true;
+        timing =true;
     }
     public void stopTime(View view){
+        this.view = view;
         //stops time
-       timing =false;
+        timing =false;
     }
     public void resetTime(View view){
+        this.view = view;
         //stops time
         timing =false;
         //resets time to 0
@@ -36,35 +40,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-        TextView timeView = findViewById(R.id.timer);
-        //instantiating handler to send and process Runnable objects to be executed at some point
-        Handler handler = new Handler();
+        TextView tView = findViewById(R.id.timer);
+        //instantiating handler to send and process Runnable objects to be executed
+        Handler handler;
+        handler = new Handler();
         handler.post(new Runnable(){
 
             @Override
             public void run(){
-                //converting seconds to hours and minutes
-                int hrs;
-                //3600 seconds in an hour
-                hrs = seconds / 3600;
-                int mins;
-                //3600/60=60 seconds which is one minute
-                mins = (seconds % 3600)/60;
+                //converting hours and minutes to seconds
                 int secs;
                 //seconds
                 secs = seconds % 60;
-                //establishing format of timer
-                String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hrs, mins, secs);
-                //setting format of timer to timeview
-                timeView.setText(time);
-                //increments seconds by 1
-                if (!timing) {
-                } else {
+                int mins;
+                //3600/60=60 seconds which is one minute
+                mins = (seconds % 3600)/60;
+                int hrs;
+                //3600 seconds in an hour
+                hrs = seconds / 3600;
+
+                if (!timing) handler.postDelayed(this, 1000);
+                else {
+                    //increments seconds by 1
                     seconds++;
+                    //increments 1 second every 1000 milliseconds
+                    handler.postDelayed(this, 1000);
                 }
-                //increments 1 second every 1000 milliseconds
-                handler.postDelayed(this, 1000);
-                }
+
+                //establishing format of timer
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d",
+                        hrs,
+                        mins,
+                        secs);
+                //setting format of timer to tView
+                tView.setText(time);
+
+            }
 
         });
     }
