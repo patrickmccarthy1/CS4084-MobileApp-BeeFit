51c7d52b60b2e4ce44b8508c80968dd66c858b11
Ben_Christie <ben.christie11@gmail.com>
2022-04-16 13:22:52 +0100

    Workouts page to display list of user created workouts

---
+++ app/src/main/java/com/example/beefit/Workouts.java
@@ -0,0 +1,123 @@
+package com.example.beefit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class Workouts extends AppCompatActivity {
+    private ImageView return_arrow;
+    private TextView workout_title;
+    private EditText create_workout;
+    private Button add_workout_button;
+    private  ListView workouts_list;
+
+    private ArrayList<String> workouts;
+    private ArrayAdapter<String> adapter;
+
+    private String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workouts);
+
+        initView();
+
+
+
+        return_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToMainPage();
+            }
+        });
+
+        add_workout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = create_workout.getText().toString();
+
+                if(name == null || name.length() == 0) {
+                    Toast.makeText(Workouts.this, "Name Your Workout", Toast.LENGTH_SHORT).show();
+                } else {
+                    addEntry(name);
+                    //reset
+                    create_workout.setText("");
+                }
+            }
+        });
+
+//        workouts_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                goToExpandedWorkoutPage();
+//                passWorkoutName(workouts_list.getItemAtPosition(i).toString());
+//            }
+//        });
+
+        //Long press to remove
+        workouts_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Workouts.this, "Removed " + workouts.get(i), Toast.LENGTH_SHORT).show();
+                removeEntry(i);
+                return false;
+            }
+        });
+    }
+
+    private void addEntry(String entry) {
+        workouts.add(entry);
+        workouts_list.setAdapter(adapter);
+        Toast.makeText(Workouts.this, entry + " Added", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void removeEntry(int remove) {
+        workouts.remove(remove);
+        workouts_list.setAdapter(adapter);
+    }
+
+    private void returnToMainPage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+//    private void goToExpandedWorkoutPage() {
+//        Intent intent = new Intent(this, ExpandedWorkout.class);
+//        startActivity(intent);
+//    }
+
+    private void initView() {
+        return_arrow = (ImageView) findViewById(R.id.return_arrow_workouts);
+        workout_title = (TextView) findViewById(R.id.user_workout_title);
+        create_workout = (EditText) findViewById(R.id.create_workout);
+        add_workout_button = (Button) findViewById(R.id.add_workout_button);
+        workouts_list = (ListView) findViewById(R.id.workouts_list);
+        workouts = new ArrayList<>();
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, workouts);
+    }
+
+//    private void passWorkoutName(String entry) {
+//        //send workout name to expanded workout
+//        Intent sendToExpanded = new Intent(this, ExpandedWorkout.class);
+//        sendToExpanded.putExtra("expandedTitle", entry);
+//        startActivity(sendToExpanded);
+//    }
+
+}
