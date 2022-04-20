95e17565a3ade88be3cf59cfa4ff921058555d52
Brian Reale <91121209+brianreale@users.noreply.github.com>
2022-04-20 16:16:34 +0100

    Merge pull request #8 project from Brian
    stopwatch,journal,map

--- app/src/main/java/com/example/beefit/MainActivity.java
+++ app/src/main/java/com/example/beefit/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     private Button stopwatch;
     private Button journal;
     private Button map;
+    private Button details;
+    private Button createWorkout;
+    private Button exWorkout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {startJournalActivity();}
         });
+        details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {details();}
+        });
+        createWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {createWorkout();}
+        });
+        exWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {expandedWO();}
+        });
     }
-    //instantiate intent so button can launch MapActivity
+    //instantiate intent so button can launch stopwatch
     private void startStopwatchActivity() {
         Intent intent = new Intent(MainActivity.this,stopwatch.class);
         startActivity(intent);
@@ -42,15 +57,33 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this,MainActivityMap.class);
         startActivity(intent);
     }
-    //instantiate intent so button can launch MapActivity
+    //instantiate intent so button can launch Journal
     private void startJournalActivity() {
         Intent intent = new Intent(MainActivity.this,MainActivityJournal.class);
+        startActivity(intent);
+    }
+    //instantiate intent so button can launch stopwatch
+    private void details() {
+        Intent intent = new Intent(MainActivity.this,Details.class);
+        startActivity(intent);
+    }
+    //instantiate intent so button can launch MapActivity
+    private void createWorkout() {
+        Intent intent = new Intent(MainActivity.this,Workouts.class);
+        startActivity(intent);
+    }
+
+    private void expandedWO() {
+        Intent intent = new Intent(MainActivity.this,ExpandedWorkout.class);
         startActivity(intent);
     }
     private void init() {
         stopwatch = (Button) findViewById(R.id.buttonPage);
         journal = (Button) findViewById(R.id.journal_Page);
         map = (Button) findViewById(R.id.maps_Page);
+        details = (Button) findViewById(R.id.Details);
+        createWorkout = (Button) findViewById(R.id.Workout);
+        exWorkout = (Button) findViewById(R.id.Expanded);
     }
 
 
